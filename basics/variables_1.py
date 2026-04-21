fcda51627e8fdad3727d97c212a7b05533fa342f
sanya <237241903+r3ven000@users.noreply.github.com>
2026-04-21 21:18:33 +0400

    new practice in variables_1.py

--- basics/variables_1.py
+++ basics/variables_1.py
@@ -1,21 +1,23 @@
 x = 5
 #In this case, x is a variable, and the = sign assigns it the value 5
-#pratice - write a variable with the value 10
+#pratice 1 - write a variable with the value 10
 y = 1+1-1*1/1//1
 #Variables can contain any of the mathematical operations available in Python, which we will discuss later.
 z = x + y
 #and so on
-#practice - 
 
 #and now to data types
+
 #Python has a function that allows the user to enter data. Example:
 x = input() #Python can overwrite variables
+print(type(x)) #The print() function displays any value on the screen, and the type() function displays the data type of the variable.
+#practice 2 - write a variable with the input of any name and birth
 #At this stage we will need only 3 data types, 1-int (from the word integer), 2-float and str (from the word string).
 y = int(input()) #int only accepts integers
 z = float(input()) #float accepts any number but formats it as a 'float' (example: 1.0)
 #The x value's data type is already str because Python sets it by default.
 #DIFFERENT DATA TYPES CANNOT INTERACT WITH EACH OTHER!
-#practice - 
+#practice 3 - Make a program that takes two numbers (int), adds them together, and outputs the result.
 
 
 
