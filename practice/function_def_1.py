e955db73e0e5ab9c5442260a2739ab0e2dcf68d8
sanya <237241903+r3ven000@users.noreply.github.com>
2026-04-23 23:11:14 +0400

    Update function_def_1.py

--- practice/function_def_1.py
+++ practice/function_def_1.py
@@ -1,1 +1,12 @@
-
+#Write a function is_palindrome(text) that checks if a string is a palindrome 
+#(reads the same forwards and backwards).
+#The function should return True or False.
+def sum_even(numbers):
+    total = 0
+    for number in numbers:
+        if number % 2 == 0:
+            total += number
+    return total
+            
+#example
+print(sum_even([1, 2, 3, 4, 5, 6]))
