3ae6c84cbb73ba44a6bb782e485987d89aadfbda
sanya <237241903+r3ven000@users.noreply.github.com>
2026-04-25 12:39:31 +0400

    Create function_def_2.py

---
+++ practice/function_def_2.py
@@ -0,0 +1,11 @@
+#Write a function that takes a number as input and returns its square.
+#Requirements:
+#Function name: `square(num)`
+#Parameter: integer or float
+#Return: the number multiplied by itself
+#Use the `**` operator (or `*`)
+def square(num):
+    return num ** 2
+
+print(square(5)) 
+print(square(3))
