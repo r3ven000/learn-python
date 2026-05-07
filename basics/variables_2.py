dd1990cf2142b2101f310042d4cb351a7a0960d5
sanya <237241903+r3ven000@users.noreply.github.com>
2026-05-07 22:48:06 +0400

    Update variables_2.py

--- basics/variables_2.py
+++ basics/variables_2.py
@@ -1,1 +1,16 @@
+#Type conversion 
 
+#Concatenation (joining strings)
+
+#practice 4 
+#The user enters a number as a string, converts it to an int, and multiplies it by 2.
+
+#Arithmetic operations with different types – how type casting works
+
+#practice 5
+#
+
+#String Formatting – f-strings Basics
+
+#practice 6
+#
