84d62508bcc65544d6c287b0c08b2489c610440f
sanya <237241903+r3ven000@users.noreply.github.com>
2026-05-08 23:46:11 +0400

    Update variables_2.py

--- basics/variables_2.py
+++ basics/variables_2.py
@@ -1,9 +1,13 @@
 #Type conversion 
 user_input = input("enter word: ") #outputs to the terminal what is in quotes and waits for user input
 print(type(user_input))
-
+#Learn more about basic data types in variables_1
 #Concatenation (joining strings)
-
+first_name = "Tom"
+last_name = "Jerry"
+#More details about mathematical operations below
+full_name = first_name + " and " + last_name
+print(full_name)
 #practice 4 
 #Create variables and make sure that when you add their values ​​(10 and 10), the product will be "1010".
 
