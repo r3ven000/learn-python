20c123f73286d27c8cb6a619afdaf5c02fa017e4
sanya <237241903+r3ven000@users.noreply.github.com>
2026-05-09 00:01:58 +0400

    Update variables_2.py

--- basics/variables_2.py
+++ basics/variables_2.py
@@ -12,11 +12,15 @@ print(full_name)
 #Create variables and make sure that when you add their values ​​(10 and 10), the product will be "1010".
 
 #Arithmetic operations with different types – how type casting works
-
+#An example of basic arithmetic operations in Python
+operation = 1 + 2 - 1 * 2 / 2 // 2 + 1 % 2 
+#+ add, - subtract, * multiply, / divide, // integer division, % remainder output
 #practice 5
-#
+#Calculate the radius of a circle if its diameter is 16 cm (6.3 inches)
 
 #String Formatting – f-strings Basics
-
+#example
+x = 5
+print(f(x = {x})) #in { } the value of a variable or something
 #practice 6
-#
+#From Practice 5, derive a variable with the radius value using f-strings
