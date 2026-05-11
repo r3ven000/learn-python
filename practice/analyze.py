a8e2d86d3f5be83caf0646769033074c9245513d
sanya <237241903+r3ven000@users.noreply.github.com>
2026-05-11 23:50:37 +0400

    Create analyze.py

---
+++ practice/analyze.py
@@ -0,0 +1,14 @@
+def analyze_stock(items):
+   total = 0
+   for item in items:
+      maxium = max(item)
+      total += item
+return total, maximum
+
+items_list = []
+
+while True:
+  user_items = int(input('enter item(or exit): '))
+     if user_items == 'exit'
+   break
+else:
