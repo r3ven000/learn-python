f40832bbc91a3d707431fd4628d8dd32fd6e7f48
sanya <237241903+r3ven000@users.noreply.github.com>
2026-05-12 21:31:33 +0400

    Create 01_hello_python.py

---
+++ practice/kaggle_progress/01_hello_python.py
@@ -0,0 +1,42 @@
+spam_amount = 0
+print(spam_amount)
+#Function calls:. print is a Python function that displays the value passed to it on the screen. 
+
+# Ordering Spam, egg, Spam, Spam, bacon and Spam (4 more servings of Spam)
+spam_amount = spam_amount + 4
+
+if spam_amount > 0:
+    print("But I don't want ANY spam!")
+
+viking_song = "Spam " * spam_amount
+print(viking_song)
+
+#There are a lot of nuances in this code, so let's look at them one by one.
+
+spam_amount = 0
+
+#Variable assignment: Here we create a variable called spam_amount and assign it the value of 0 using =
+#which is called the assignment operator.
+
+print(spam_amount)
+
+#Function calls:. print is a Python function that displays the value passed to it on the screen.
+#We call functions by putting parentheses after their name, and putting the inputs (or arguments) to the function in those parentheses.
+
+# Ordering Spam, egg, Spam, Spam, bacon and Spam (4 more servings of Spam)
+spam_amount = spam_amount + 4
+
+#The first line above is a comment. In Python, comments begin with the # symbol.
+
+if spam_amount > 0:
+    print("But I don't want ANY spam!")
+
+viking_song = "Spam Spam Spam"
+print(viking_song)
+
+#The colon (:) at the end of the if line indicates that a new code block is starting. Subsequent lines which are indented are part of that code block.
+
+viking_song = "Spam " * spam_amount
+print(viking_song)
+
+
