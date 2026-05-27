88242e41ea3366ed0d17146862303d8c657a1f7c
sanya <237241903+r3ven000@users.noreply.github.com>
2026-05-27 19:59:10 +0400

    Update operator_if.py

--- basics/02_conditional_operators/operator_if.py
+++ basics/02_conditional_operators/operator_if.py
@@ -1,1 +1,16 @@
+#The if statement performs an action based on a condition.
+#For example
+x = 5
+y = 2
+if x + y == 7:
+#A tab is needed here because the actions take place inside the condition ( : )
+  print("x and y = 7!")
 
+if x * y == 10:
+  print("x * y = 10!")
+
+#The condition may contain not only the print() function but also much more
+
+#practice: Write a program that uses an if condition to check the equality of any variable.
+
+#practice: Write a program that receives two digits as input into two variables and, if they are even, displays their result. If they are odd, displays "odd."
