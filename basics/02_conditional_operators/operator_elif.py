5aea494daae34a93077cf9a04eff8318f92493e7
sanya <237241903+r3ven000@users.noreply.github.com>
2026-05-28 21:54:13 +0400

    Create operator_elif.py

---
+++ basics/02_conditional_operators/operator_elif.py
@@ -0,0 +1,9 @@
+#this operator use if 
+x = int("please enter integer number 1: ")
+y = int("please enter integer number 2: ")
+if x + y % 2 == 0:
+  print(f"x and y even!")
+
+elif x + y % 2 != 0:
+  print(f"x and y no even!")
+  
