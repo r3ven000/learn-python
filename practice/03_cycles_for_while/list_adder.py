0c3ae1a9bed889f33cc4675c242e4675451f2ebe
sanya <237241903+r3ven000@users.noreply.github.com>
2026-05-29 22:09:12 +0400

    Create list_adder.py

---
+++ practice/03_cycles_for_while/list_adder.py
@@ -0,0 +1,10 @@
+numbers = []
+while True:
+    uniq_num = input('enter uniq num(or exit): ')
+    if uniq_num == 'exit':
+        break
+    elif uniq_num not in numbers:
+        numbers.append(uniq_num)
+        print(f'num uniq and add! list: {numbers}')
+    else:
+        print('num no uniq!')
