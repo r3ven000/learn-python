b86199e6eeabdb4af74daaf323dfd88b1313f029
sanya <237241903+r3ven000@users.noreply.github.com>
2026-06-04 22:22:23 +0400

    Update 06_todo.py

--- practice/02_def/06_todo.py
+++ practice/02_def/06_todo.py
@@ -1,3 +1,4 @@
+#imports
 import pyfiglet
 import os
 import time
@@ -5,23 +6,33 @@ welcome = pyfiglet.figlet_format('POMITRACK')
 
 todo_list = [0]
 
-
+#todo function
 def create_list(todo):
     while True:
-        width = os.get_terminal_size().columns
+        width = os.get_terminal_size().columns #setting
 
-        queston = input('create new task or exit?: ')
-        if queston == 'exit':
-            print(f'today to-do list made up:')
-            for index, task in enumerate(todo):
-                if task != 0:
-                    print(f'{index}. {task}'.center(width))
+        multi_task = input('task / del / edit / exit : '.center(width)).strip()
+        
+        parts = multi_task.split()
+        if not parts:
+            continue
+        command = parts[0].lower()
+
+        if command == 'exit':
             break
-        else:
-            name_task = input('task / del / edit / exit ').strip()
-            if name_task == 'delete':
-                deleter = int(input('which task should I delete?: '))
-                todo.pop(deleter)
+
+        elif command == 'del':
+
+            if len(parts) > 1:
+
+                idx = int(parts[1])
+                todo.pop(idx)
+
+        elif command == 'edit':
+            if len(parts) > 1:
+                idx = int(parts[1])
+                new_text_task = input('enter new text task: ')
+                todo[idx] = new_text_task
             else:
                 todo.append(name_task)
                 print("\n" + " TASKS ".center(width, "="))
@@ -45,10 +56,12 @@ def timer(minutes):
         time.sleep(1)
         seconds -= 1
     print("Time's up".center(width))
-
+def pomidoro(todo):
+    timer(25)
+    timer(5)
 manager = {
     't': create_list,
-    'p': lambda todo: timer(25)
+    'p': pomidoro
 }
 width = os.get_terminal_size().columns
 
@@ -71,5 +84,7 @@ while True:
     else:
         print('command not found!'.center(width))
 print(welcome)
+
+
 
 
