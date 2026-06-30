5e44515accbf4db5424a0dae02610c2e447558a6
sanya <237241903+r3ven000@users.noreply.github.com>
2026-06-30 21:42:05 +0400

    Create function_def_6.py

---
+++ practice/02_def/function_def_6.py
@@ -0,0 +1,19 @@
+#1
+
+def age_control(age):
+    if age < 18:
+        return False
+    elif age <= 1:
+        return 'ERROR'
+    else:
+        return True
+    print(bool)
+
+contol = int(input('enter your age: '))
+
+age_control(contol)
+
+#2
+
+def system_edits_documents(user, file, lock):
+    if file not is_locked and user == 
