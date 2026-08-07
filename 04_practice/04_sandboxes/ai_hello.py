b363160747d553ce46d4a58bda37878bb49d0073
sanya <237241903+r3ven000@users.noreply.github.com>
2026-08-07 22:47:37 +0400

    Update ai_hello.py

--- 04_practice/04_sandboxes/ai_hello.py
+++ 04_practice/04_sandboxes/ai_hello.py
@@ -1,24 +1,34 @@
-VOCAB = {"п":1, "р":2, "и":3, "в":4, "е":5, "т":6, " ":7}
+import numpy
+#step 1
+
+VOCAB = {"п":1, "р":2, "и":3, "в":4, "е":5, "т":6, " ":7} #dict
 
 DATA = [
-    ("привет", "привет"),   # вход → желаемый выход
+    ("привет", "привет"),   # вход → input
     ("пока",   "пока"),
     ("впит",   "впит"),     # повторы букв — чтобы сеть не запомнила одно слово
     ("пвт",    "пвт"),
 ]
 
-def word_to_codes(word):
+def word_to_codes(word): #translate word to numbers(codes)
     codes = []
     for i in word:
-        codes.append(word[i])
+        codes.append(VOCAB[i])
     return codes
 
-def codes_to_onehot(codes):
+def codes_to_onehot(codes): #matrix
+    result = []
     for code in codes:
-        vector = [0]
-        vector[0]*7
-        print(vector)
+        vector = [0] * 7
+        vector[code - 1] = 1
+        result.append(vector)
+    return result
 
-word_to_codes(VOCAB)
-codes_to_onehot(codes)
+#step 2
+
+N = 10
+W1 = np.random.uniform(-0.5, 0.5, (7, N))
+b1 = np.random.uniform(-0.5, 0.5, (1, N))
+W2 = np.random.uniform(-0.5, 0.5, (N, 7))
+b2 = np.random.uniform(-0.5, 0.5, (1, 7))
 
